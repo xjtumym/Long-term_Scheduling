e19226a4f20b824341344867beb80ddf2f46aff7
xjtumym <112613720+xjtumym@users.noreply.github.com>
2022-11-03 16:01:54 +0800

    introduce gurobi

--- test.py
+++ test.py
@@ -1,1 +1,29 @@
-import numpy
+# Solve the following MIP:
+#  maximize
+#        x +   y + 2 z
+#  subject to
+#        x + 2 y + 3 z <= 4
+#        x +   y       >= 1
+#        x, y, z binary
+
+import gurobipy as gp
+
+# Create a new model
+m = gp.Model()
+
+# Create variables
+x = m.addVar(vtype='B', name="x")
+y = m.addVar(vtype='B', name="y")
+z = m.addVar(vtype='B', name="z")
+
+# Set objective function
+m.setObjective(x + y + 2 * z, gp.GRB.MAXIMIZE)
+
+# Add constraints
+m.addConstr(x + 2 * y + 3 * z <= 4)
+m.addConstr(x + y >= 1)
+
+# Solve it!
+m.optimize()
+
+print(f"Optimal objective value: {m.objVal}")
